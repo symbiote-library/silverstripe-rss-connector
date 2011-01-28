25830bcc63999204e611250fe02a9b12dd10360c
ajshort <andrewjshort@gmail.com>
2011-01-28 15:38:42 +1100

    Added the RSS content source and item classes with the basic ability to import RSS feed items.

---
+++ code/model/RssContentItem.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * An RSS entry from an external RSS feed.
+ *
+ * @package silverstripe-rssconnector
+ */
+class RssContentItem extends ExternalContentItem {
+
+	protected $item;
+	protected $categories;
+
+	/**
+	 * @param RssContentSource $source
+	 * @param SimplePie_Item $item
+	 */
+	public function __construct($source = null, $item = null) {
+		if (is_object($item)) {
+			$this->item = $item;
+			$item = $item->get_id();
+		}
+
+		parent::__construct($source, $item);
+	}
+
+	public function init() {
+		$this->Title     = $this->item->get_title();
+		$this->Link      = $this->item->get_link();
+		$this->Date      = $this->item->get_date('Y-m-d H:i:s');
+		$this->Content   = $this->item->get_content();
+
+		if ($author = $this->item->get_author()) {
+			$this->AuthorName  = $author->get_name();
+			$this->AuthorEmail = $author->get_email();
+			$this->AuthorLink  = $author->get_link();
+		}
+
+		$this->categories = new DataObjectSet();
+		$categories = @$this->item->get_categories();
+
+		if ($categories) foreach ($categories as $category) {
+			$this->categories->push(new ArrayData(array(
+				'Label'  => $category->get_label(),
+				'Term'   => $category->get_term(),
+				'Scheme' => $category->get_scheme()
+			)));
+		}
+
+		$this->Latitude  = $this->item->get_latitude();
+		$this->Longitude = $this->item->get_longitude();
+	}
+
+	public function numChildren() {
+		return 0;
+	}
+
+	public function stageChildren() {
+		return new DataObjectSet();
+	}
+
+	public function getType() {
+		return 'file';
+	}
+
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+
+		$categories = new TableListField('Categories', false, array(
+			'Label'  => 'Label',
+			'Term'   => 'Term',
+			'Scheme' => 'Scheme'
+		));
+		$categories->setCustomSourceItems($this->categories);
+
+		$fields->addFieldsToTab('Root.Details', array(
+			new HeaderField('CategoriesHeader', 'Categories', 4),
+			$categories->performReadonlyTransformation()
+		));
+
+		$fields->addFieldsToTab('Root.Location', array(
+			new ReadonlyField('Latitude', null, $this->Latitude),
+			new ReadonlyField('Longitude', null, $this->Longitude)
+		));
+
+		$fields->addFieldToTab('Root.Behaviour', new ReadonlyField(
+			'ShowInMenus', null, $this->ShowInMenus
+		));
+
+		return $fields;
+	}
+
+}
---
+++ code/model/RssContentSource.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @package silverstripe-rsscontent
+ */
+
+require_once 'simplepie/simplepie.inc';
+
+/**
+ * An external RSS feed that content can be imported from.
+ *
+ * @package silverstripe-rsscontent
+ */
+class RssContentSource extends ExternalContentSource {
+
+	public static $db = array(
+		'Url' => 'Varchar(255)'
+	);
+
+	protected $client;
+
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+
+		$fields->addFieldToTab(
+			'Root.Main', new TextField('Url', 'RSS/Atom Feed URL'),
+			'ShowContentInMenu'
+		);
+
+		return $fields;
+	}
+
+	/**
+	 * Attempts to get an RSS content item by GUID.
+	 *
+	 * @param  string|int $id
+	 * @return RssContentItem
+	 */
+	public function getObject($id) {
+		$id    = $this->decodeId($id);
+		$items = $this->getClient()->get_items();
+
+		foreach ($items as $item) {
+			if ($item->get_id() == $id) return new RssContentItem($this, $item);
+		}
+	}
+
+	public function getRoot() {
+		return $this;
+	}
+
+	public function stageChildren() {
+		$items    = $this->getClient()->get_items();
+		$children = new DataObjectSet();
+
+		foreach ($items as $item) {
+			$children->push(new RssContentItem($this, $item));
+		}
+
+		return $children;
+	}
+
+	/**
+	 * @return SimplePie
+	 */
+	public function getClient() {
+		if (!$this->client) {
+			$this->client = new SimplePie($this->Url);
+			$this->client->enable_cache(true);
+			$this->client->set_cache_location(TEMP_FOLDER);
+		}
+
+		return $this->client;
+	}
+
+}
